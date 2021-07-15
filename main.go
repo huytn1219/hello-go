ed271b97059368ff2077d9ece8f2442777b41af6
huytn1219 <huy.nguyen1219@gmail.com>
2021-07-15 09:49:07 -0700

    inital commit

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ func main() {
 	http.ListenAndServe(":8080", nil)
 }
 
+// HelloServer return Hello World string
 func HelloServer(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "!...Hello World...!")
 }
